b3d40b44621799ee7599e56b086b7e8accb4ab56
Marcus <marcus@baby-xero.local>
2016-02-27 22:16:25 +0100

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Marcus on 21/02/16.
+//  Copyright © 2016 marcus@xerbot.org. All rights reserved.
+//
+
+#include <iostream>
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
